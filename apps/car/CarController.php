78b8ef7fdad35cd44175274f25336f15d42709a7
Nikita Nyatin <yesnik@bank24.ru>
2014-09-18 14:05:39 +0600

    Обработка переменных в шаблонах

--- apps/car/CarController.php
+++ apps/car/CarController.php
@@ -19,8 +19,17 @@ class CarController extends Controller
     public function index ()
     {
         global $db;
+        global $SITE_ROOT;
         //$rows = $this->model->getAll();
-        $row = $this->model->getById(2);
-        var_dump($row);
+        $templateVars = $this->model->getById(2);
+        $templateVars['title'] = 'Заголовок страницы';
+        //var_dump($row);
+        $template = $SITE_ROOT . 'templates/base.html.php';
+        $raw_html = file_get_contents($template);
+
+        foreach($templateVars as $key => $value) {
+            $raw_html = preg_replace('/{{\s*'.$key.'\s*}}/', $value, $raw_html);
+        }
+        echo $raw_html;
     }
 }
