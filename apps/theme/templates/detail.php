d67fe4c7d5b364dbd379fb6f473ae0646789f0c6
Nikita Nyatin <yesnik@bank24.ru>
2014-09-19 17:02:59 +0600

    Вывод сообщения при отсутствии комментариев

--- apps/theme/templates/detail.php
+++ apps/theme/templates/detail.php
@@ -5,8 +5,12 @@
 
 <h3>Комментарии</h3>
 
+<?php if (!empty($vars['comments'])){ ?>
 <ul>
-<?php foreach ($vars['comments'] as $comment): ?>
-    <li><?= $comment['comment']; ?></li>
-<?php endforeach ?>
+    <?php foreach ($vars['comments'] as $comment): ?>
+        <li><?= $comment['comment']; ?></li>
+    <?php endforeach ?>
 </ul>
+<?php } else { ?>
+    <p>Пока никто не оставил комментарий. Вы будете первым.</p>
+<?php } ?>
