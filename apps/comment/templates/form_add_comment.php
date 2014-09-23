1700b77e1111becf260baaa0fc32b09523c80f7e
Nikita Nyatin <yesnik@bank24.ru>
2014-09-23 12:01:45 +0600

    Оформление форм вынесено в отдельный файл

--- apps/comment/templates/form_add_comment.php
+++ apps/comment/templates/form_add_comment.php
@@ -6,13 +6,13 @@
     <form action="/comment/create" method="post" xmlns="http://www.w3.org/1999/html">
 
         <p>
-            <label class="form-add-comment__label" for="name">Имя</label><br>
-            <input class="form-add-comment__input" type="text"
+            <label class="label" for="name">Имя</label><br>
+            <input class="form-add-comment__input input" type="text"
                    name="name" placeholder="Ваше имя" value="<?= $vars['formAddComment']['name'] ?>" />
         </p>
         <p>
-            <label class="form-add-comment__label" for="comment">Комментарий</label><br>
-            <textarea class="form-add-comment__text" name="comment"
+            <label class="label" for="comment">Комментарий</label><br>
+            <textarea class="form-add-comment__text textarea" name="comment"
                       placeholder="Ваш комментарий"><?= $vars['formAddComment']['comment'] ?></textarea>
         </p>
         <p>
--- apps/theme/templates/form_add_theme.php
+++ apps/theme/templates/form_add_theme.php
@@ -6,19 +6,19 @@
     <form action="/theme/create" method="post">
 
         <p>
-            <label class="form-add-comment__label" for="name">Ваше имя</label><br>
-            <input class="form-add-comment__input" type="text"
+            <label class="label" for="name">Ваше имя</label><br>
+            <input class="form-add-comment__input input" type="text"
                    name="name" placeholder="Ваше имя" value="<?= $vars['formAddTheme']['name'] ?>" />
         </p>
         <p>
-            <label class="form-add-comment__label" for="title">Заголовок темы</label><br>
-            <input class="form-add-comment__input" type="text"
-                   name="title" placeholder="Ваше имя" value="<?= $vars['formAddTheme']['title'] ?>" />
+            <label class="label" for="title">Заголовок темы</label><br>
+            <input class="form-add-comment__input input" type="text"
+                   name="title" placeholder="Заголовок темы" value="<?= $vars['formAddTheme']['title'] ?>" />
         </p>
         <p>
-            <label class="form-add-comment__label" for="content">Содержание</label><br>
-            <textarea class="form-add-comment__text" name="content"
-                      placeholder="Ваш комментарий"><?= $vars['formAddTheme']['content'] ?></textarea>
+            <label class="label" for="content">Содержание</label><br>
+            <textarea class="form-add-comment__text textarea" name="content"
+                      placeholder="Текст сообщения"><?= $vars['formAddTheme']['content'] ?></textarea>
         </p>
         <p>
             <input type="submit" value="Добавить"/>
