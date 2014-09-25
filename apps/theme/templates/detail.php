e1d8272e48ddf74781c6cda06ed555b8f7c15a3c
Nikita Nyatin <yesnik@bank24.ru>
2014-09-25 15:25:04 +0600

    Для одной страницы пагинация не выводится

--- apps/theme/templates/detail.php
+++ apps/theme/templates/detail.php
@@ -8,37 +8,42 @@
 
 <h2 class="page__h2 page__h2_underline">Комментарии (<?= $vars['theme']['comments_num'] ?>)</h2>
 
-<?php if (!empty($vars['comments'])){ ?>
-<ul id="comments_holder" class="comment">
-    <?php foreach ($vars['comments'] as $comment): ?>
-        <li class="comment__item">
-            <div class="comment__left-col">
-                <span title="О пользователе" class="comment__user">
-                    <?= $comment['name']; ?>
-                </span>
-                <div class="comment__time">
-                    <?= date("d.m.Y в H:i", strtotime($comment['created_at'])) ?>
-                </div>
-                <div class="comment__user-info">
-                    С нами с: <?= date("d.m.Y", strtotime($comment['user_created_at'])) ?><br />
-                    Сообщений: <?= $comment['user_comments_num'] ?>
-                </div>
-            </div>
+<?php if (!empty($vars['comments'])) { ?>
 
-            <div class="comment__right-col">
-                <?= $comment['comment']; ?>
-            </div>
-        </li>
-    <?php endforeach ?>
-</ul>
-<ul class="pagination">
-    <?php foreach($vars['pages'] as $i => $pageUrl):  ?>
-        <li class="pagination__item">
-            <a class="pagination__link <?php if ($vars['page_current'] == $i + 1) echo "pagination__link_active"?>"
-               href="<?= $pageUrl ?>"><?= ($i + 1) ?></a>
-        </li>
-    <?php endforeach ?>
-</ul>
+    <ul id="comments_holder" class="comment">
+        <?php foreach ($vars['comments'] as $comment): ?>
+            <li class="comment__item">
+                <div class="comment__left-col">
+                    <span title="О пользователе" class="comment__user">
+                        <?= $comment['name']; ?>
+                    </span>
+                    <div class="comment__time">
+                        <?= date("d.m.Y в H:i", strtotime($comment['created_at'])) ?>
+                    </div>
+                    <div class="comment__user-info">
+                        С нами с: <?= date("d.m.Y", strtotime($comment['user_created_at'])) ?><br />
+                        Сообщений: <?= $comment['user_comments_num'] ?>
+                    </div>
+                </div>
+
+                <div class="comment__right-col">
+                    <?= $comment['comment']; ?>
+                </div>
+            </li>
+        <?php endforeach ?>
+    </ul>
+
+    <?php if(sizeof($vars['pages']) > 1): ?>
+        <ul class="pagination">
+            <?php foreach($vars['pages'] as $i => $pageUrl):  ?>
+                <li class="pagination__item">
+                    <a class="pagination__link <?php if ($vars['page_current'] == $i + 1) echo "pagination__link_active"?>"
+                       href="<?= $pageUrl ?>"><?= ($i + 1) ?></a>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    <?php endif; ?>
+
 <?php } else { ?>
     <p>Пока никто не оставил комментарий. Вы будете первым.</p>
 <?php } ?>
