3ccae8ad6e3694c14a0a75f4c7a28ce94ff217f8
Nikita Nyatin <yesnik@yandex.ru>
2015-03-19 11:24:16 +0500

    Refactor autoload paths

--- main/autoloader.php
+++ main/autoloader.php
@@ -1,14 +1,21 @@
 <?php
-spl_autoload_register(function($class){
-  global $installedApps;
-  global $SITE_ROOT;
-  
-  foreach ($installedApps as $appName) {
-    $includePath = $SITE_ROOT . DIRECTORY_SEPARATOR . 'apps' . 
-      DIRECTORY_SEPARATOR . $appName;
-    set_include_path(get_include_path(). PATH_SEPARATOR . $includePath); 
-  }
-  $fileName = ucfirst( end( (explode('\\', $class)) ));
 
+//Пути для автозагрузки системные
+$includePaths = array(
+  'utils'
+);
+foreach ($includePaths as $path) {
+  set_include_path(get_include_path(). PATH_SEPARATOR . $path);
+}
+
+//Пути для автозагрузки на основе установленных приложений
+foreach ($installedApps as $appName) {
+  $includePath = $SITE_ROOT . DIRECTORY_SEPARATOR . 'apps' . 
+    DIRECTORY_SEPARATOR . $appName;
+  set_include_path(get_include_path(). PATH_SEPARATOR . $includePath); 
+}
+
+spl_autoload_register(function($class){
+  $fileName = ucfirst( end( (explode('\\', $class)) ));
   require_once $fileName . '.php';
 });
--- main/bootstrap.php
+++ main/bootstrap.php
@@ -1,18 +1,11 @@
 <?php
 
-require 'config/config.php';
+require './config/config.php';
+require './config/installedApps.php';
 require 'debug.php';
 require 'autoloader.php';
 
-require 'Controller.php';
-require 'Model.php';
-require 'View.php';
-require './config/installedApps.php';
-
-require './utils/Utils.php';
 require './utils/templateHelpers.php';
-require './utils/FlashMessage.php';
-require './utils/Paginator.php';
 
 require './apps/comment/utils/CommentPaginator.php';
 require './apps/theme/utils/ThemePaginator.php';
