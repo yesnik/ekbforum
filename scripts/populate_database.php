aa352b4c2b052aa819ffc0da67ba78799005fc30
Nikita Nyatin <yesnik@yandex.ru>
2015-05-30 19:48:57 +0500

    Add batchSize param in populate script

--- scripts/populate_database.php
+++ scripts/populate_database.php
@@ -9,27 +9,62 @@ use utils\ParseArgv;
 
 $db = Database::getConnection();
 $pop = new DatabasePopulator();
-
 $args = (new ParseArgv())->params();
+
+/*
+There are errors on inserting big amount of data in DB:
+- Warning: PDO::query(): MySQL server has gone away in ...
+- Warning: PDO::query(): Error reading result set's header in ...
+So we will insert rows by batches
+*/
+$batchSize = array_key_exists('batch', $args) && is_numeric($args['batch']) ? $args['batch'] : 800;
+
 $themesToInsert = array_key_exists('n', $args) && is_numeric($args['n']) ? $args['n'] : 11;
 
-$query = 'INSERT INTO themes (user_id, title, content, created_at, comments_num) VALUES ';
-while ($themesToInsert > 0) {
-    $query .= '("' .
-      implode('", "', array(
-          $pop->getInt(1, 20),
-          $pop->getString(),
-          $pop->getText(),
-          $pop->getDatetime(),
-          $pop->getInt()
-        )) . '"),';
+function insertRows($amount)
+{
+    global $db;
+    global $pop;
 
-    $themesToInsert--;
+    $query = 'INSERT INTO themes (user_id, title, content, created_at, comments_num) VALUES ';
+    while ($amount > 0) {
+        $query .= '("' .
+          implode('", "', array(
+              $pop->getInt(1, 20),
+              $pop->getString(),
+              $pop->getText(),
+              $pop->getDatetime(),
+              $pop->getInt()
+            )) . '"),';
+
+        $amount--;
+    }
+    $query = rtrim($query, ',');
+
+    $stmt = $db->query($query);
+
+    if ($stmt) {
+        echo "In table 'themes' was successfully inserted: " . $stmt->rowCount() . " rows\n";
+    }
 }
-$query = rtrim($query, ',');
 
-$stmt = $db->query($query);
+$processed = 0;
 
-if ($stmt) {
-    echo "In table 'themes' was successfully inserted: " . $stmt->rowCount() . " rows";
+if ( $themesToInsert < $batchSize ) {
+    insertRows($themesToInsert);
+} else {
+    $steps = floor($themesToInsert / $batchSize);
+    echo 'Steps: '. $steps . "\n";
+    if ($steps < 2) {
+        insertRows($batchSize);
+        insertRows($themesToInsert - $batchSize);
+    } else {
+        $stepsDone = 0;
+        while ($stepsDone < $steps) {
+            insertRows($batchSize);
+            $stepsDone++;
+            sleep(3);
+        }
+        insertRows($themesToInsert - ($batchSize * $steps));
+    }
 }
